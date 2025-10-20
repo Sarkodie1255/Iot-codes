e78aea26ca9638600d86436aa7e946c06587b980
Sarkodie1255 <sarkcess099@gmail.com>
2025-10-20 22:51:43 +0000

    Add files via upload

---
+++ PushButtonLight.ino
@@ -0,0 +1,24 @@
+const int buttonPin=5;
+const int ledPin=4;
+
+int buttonState = 0;
+
+void setup() {
+ Serial.begin(115200);
+ pinMode(buttonPin, INPUT);
+ pinMode(ledPin, OUTPUT);
+
+
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  if (buttonState == HIGH){
+    digitalWrite(ledPin,HIGH);
+  }
+  else{
+    digitalWrite(ledPin,LOW);
+  }
+
+}
